22139b2433e94f0dbdd83c5c41c000e55ce0b917
Mikey Johnson <50218184+MikeySnowHydro@users.noreply.github.com>
2020-01-24 11:35:33 -0800

    Add files via upload

--- Single_Cell_Time_Series.R
+++ Single_Cell_Time_Series.R
@@ -4,7 +4,7 @@
 ### This function organizes a timeseries of all the variables in a .grads file from SnowModel.
 ### 
 ### Written by: Mikey Johnson, University of Nevada Reno, < mikeyj@nevada.unr.edu >
-### last eddited 01-23-2020
+### last eddited January 24, 2020
 #####################################################################################################
 
 ### Sourcing functions to read .grad files ##########################################################
@@ -15,22 +15,28 @@ source_url("https://raw.githubusercontent.com/mpaiao/ED2/master/R-utils/gridt.r"
 source_url("https://raw.githubusercontent.com/mpaiao/ED2/master/R-utils/readgrads.r")
 
 ### Building the function ###########################################################################
-Single_Cell_Timeseries <- function(filepath="snowpack.ctl",x=1,y=1){
-  
+Single_Cell_Timeseries <- function(filepath="snowpack.ctl", x=1, y=1){
   snowpack.info <- readctl(filepath)                     # Reading snowpack
-  names <- snowpack.info$varname
-  dates <- snowpack.info$gtime
-  df <- data.frame(matrix(NA, nrow = length(dates), ncol = 1 + length(names)))
-  df[,1] <- dates
+  xmax <- snowpack.info$xmax
+  ymax <- snowpack.info$ymax
   
-  for (i in 1:length(names)){
-    dat <- readgrads(vari=names[i],info=snowpack.info)
-    df[,i+1] <- dat[[9]][,1,x,y]
+  if (x > xmax | y > ymax){
+    print(paste("Grid cell out of range.", "xmax =", xmax, ", ymax =", ymax, sep=" "))
+  } else {
+    names <- snowpack.info$varname
+    dates <- snowpack.info$gtime
+    df <- data.frame(matrix(NA, nrow = length(dates), ncol = 1 + length(names)))
+    df[,1] <- dates
+    
+    for (i in 1:length(names)){
+      dat <- readgrads(vari=names[i],info=snowpack.info)
+      df[,i+1] <- dat[[9]][,1,x,y]
+    }
+    
+    names(df) <- c("Date/Time",names)
+    return(df)
   }
-  
-  names(df) <- c("Date/Time",names)
-  return(df)
 }
 
 ### Example of how to run the function #########################
-#df <- Single_Cell_Timeseries("snowpack.ctl",1,1)
+# df <- Single_Cell_Timeseries(filepath="snowpack.ctl", x=1, y=1)
---
+++ Time_Slice.R
@@ -0,0 +1,41 @@
+#####################################################################################################
+### Full grid Time Slice for SnowModel (Function)
+###
+### This function creats a matrix of the entire SnowModel extent for a specifice time step and vairable
+### 
+### Written by: Mikey Johnson, University of Nevada Reno, < mikeyj@nevada.unr.edu >
+### last eddited January 24, 2020
+#####################################################################################################
+
+### Sourcing functions to read .grad files ##########################################################
+# this code sources the files from Marcos Longo's GitHub page
+source_url("https://raw.githubusercontent.com/mpaiao/ED2/master/R-utils/readctl.r")
+source_url("https://raw.githubusercontent.com/mpaiao/ED2/master/R-utils/gridp.r")
+source_url("https://raw.githubusercontent.com/mpaiao/ED2/master/R-utils/gridt.r")
+source_url("https://raw.githubusercontent.com/mpaiao/ED2/master/R-utils/readgrads.r")
+
+### Building the function ###########################################################################
+
+## Temporarly setting a working directory for the SM data
+#setwd("~/Documents/R Scripts/SnowModel/Output File Scripts/Example_Data/MRB_Singel_Cell")
+
+Time_Slice <- function(filepath="snowpack.ctl",variable="swed",timestep){
+  snowpack.info <- readctl(filepath)                      # Reading snowpack
+  names <- snowpack.info$varname                          # Loading the variable names
+  tmax <- snowpack.info$tmax
+  
+  if (variable %in% names == FALSE){
+    print(c("Variable is not in .ctl file.  Available variables = ", paste(names, sep=",")))
+  } else if ((timestep <= tmax & 0 < timestep) == FALSE){
+    print(c("time step value is out of range", paste("Max Timestep =",tmax)))
+  } else {
+    dat <- readgrads(vari=variable,info=snowpack.info)      # Loading varable
+    slice <- dat[[9]][timestep,1,,]                         # Time Slice
+  }
+}
+
+
+### Example test function ###########################################################################
+# test <- Time_Slice(filepath = "snowpack.ctl", variable = "swed", timestep = 183)
+
+
